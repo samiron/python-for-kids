465daea1c4d1d909bee72230df7738426194a384
Lam Ho <lamho86@gmail.com>
2025-11-23 10:33:13 -0400

    More codes from Khoi

---
+++ khoi/Learning.py
@@ -0,0 +1,17 @@
+# if 10<2:   
+#     print("Your computer is broken. You need to get that checked out.")
+#     print("10 is less then2 .")
+
+# number=input("gimme a number")
+# name = input("gimme your name")
+# numbertwo=int(number)
+# numberthree=numbertwo%3
+# if  numberthree==0 and name=="Khoi":
+#     print("Your number was divisible by four")
+# else:
+#     print("Either your number is not divisible by four or you are not Khoi")
+
+choco=int(input("How m u c h c h o c alates do you  have?"))
+chocoperperpersone=int(choco/4)
+chocopeadd=choco%4
+print("Each person gets",chocoperperpersone, "with",chocopeadd,"left over.")
---
+++ khoi/fun.py
@@ -0,0 +1,17 @@
+# time="6:00AM"
+# awake=input("Do you wanna wake up yet? yes/Any other text")
+# if awake=="yes":
+#     time="6:05AM"
+#     teethbrushing=input("It is 6:05AM. DO you want to go brush your teeth yet? yes/Any other text")
+#     if teethbrushing="yes":
+#         time="6:07AM"
+#         clothes changing
+
+print("The password is the wizard... He saved us from V**d*m**t \n Enter the password... and you will learn to type...  If you are wrong... No typing! \n No capitals... Or the deal's off!")
+password=input("Enter...here!!!")
+if password=="harry potter":
+    print("Here's the typing lessons I promised:https://www.typing.com")
+else:
+    print("No typing for you!!!!!!!!! Hahahahahahahhahahahhahahhahahha")
+
+
